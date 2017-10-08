811a997ac9bd5f811c358e081a5f36c0b075f160
rijalul <muhammadrijalulmutaqin@gmail.com>
2017-10-09 06:46:53 +0700

    Initial Commit

---
+++ resources/assets/js/bootstrap.js
@@ -0,0 +1,57 @@
+
+window._ = require('lodash');
+
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+window.$ = window.jQuery = require('jquery');
+
+/**
+ * Vue is a modern JavaScript library for building interactive web interfaces
+ * using reactive data binding and reusable components. Vue's API is clean
+ * and simple, leaving you to focus on building your next great project.
+ */
+/*
+window.Vue = require('vue');
+*/
+window.axios = require('axios');
+/*
+require('vue-resource');
+*/
+
+window.axios.deaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+/**
+ * We'll register a HTTP interceptor to attach the "CSRF" header to each of
+ * the outgoing requests issued by this application. The CSRF middleware
+ * included with Laravel will automatically verify the header's value.
+ */
+
+// Vue.http.interceptors.push((request, next) => {
+//     request.headers.set('X-CSRF-TOKEN', Laravel.csrfToken);
+
+//     next();
+// });
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if(token)
+{
+	window.axios.deaults.headers.common['X-CSRF-TOKEN'] = token.content;
+}else{
+	console.error('csrf token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+// import Echo from "laravel-echo"
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: 'your-pusher-key'
+// });
